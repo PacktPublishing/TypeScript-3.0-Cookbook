4e28118c5d22652c3d89fed4f62c7dddbac9f685
ohanlon <pete.ohanlon@gmail.com>
2019-09-25 13:19:05 +0100

    Pushing the updated KMP version.

--- chapter2/recipe9/kmp.ts
+++ chapter2/recipe9/kmp.ts
@@ -48,57 +48,6 @@ export class KnuthMorrisPratt {
   }
 }
 
-function buildPatternTable(word:string) {
-  const patternTable = [0];
-  let prefixIndex = 0;
-  let suffixIndex = 1;
-
-  while (suffixIndex < word.length) {
-    if (word[prefixIndex] === word[suffixIndex]) {
-      patternTable[suffixIndex] = prefixIndex + 1;
-      suffixIndex += 1;
-      prefixIndex += 1;
-    } else if (prefixIndex === 0) {
-      patternTable[suffixIndex] = 0;
-      suffixIndex += 1;
-    } else {
-      prefixIndex = patternTable[prefixIndex - 1];
-    }
-  }
-
-  return patternTable;
-}
-
-
-export default function knuthMorrisPratt(text: string, word: string) {
-  if (word.length === 0) {
-    return 0;
-  }
-
-  let textIndex = 0;
-  let wordIndex = 0;
-
-  const patternTable = buildPatternTable(word);
-
-  while (textIndex < text.length) {
-    if (text[textIndex] === word[wordIndex]) {
-      // We've found a match.
-      if (wordIndex === word.length - 1) {
-        return (textIndex - word.length) + 1;
-      }
-      wordIndex += 1;
-      textIndex += 1;
-    } else if (wordIndex > 0) {
-      wordIndex = patternTable[wordIndex - 1];
-    } else {
-      wordIndex = 0;
-      textIndex += 1;
-    }
-  }
-
-  return -1;
-}
-
 var test = 'I love';
 
 var string = `Cupcake ipsum dolor sit amet sesame snaps. Sweet tiramisu cupcake liquorice I loveI love cheesecake candy topping chocolate cake. Muffin fruitcake dessert apple pie. Caramels brownie chupa chups marzipan jujubes jelly beans sesame snaps I love. Gummies sesame snaps powder toffee dessert ice cream. Sweet caramels tart.
